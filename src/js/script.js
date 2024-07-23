5eb37b3607ed060f3c567b34a55e6e9966fb355c
Geane Ramos <geaneramos1995@gmail.com>
2024-07-23 07:48:33 -0300

    refactor: js modules

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+import initSmoothScroll from "./scroll-suave.js";
+import initScrollAnimation from "./scroll-animacao.js";
+import initAccordion from "./accordion.js";
+import initTabNav from "./tabnav.js";
+import initModal from "./modal.js";
+
+initSmoothScroll();
+initScrollAnimation();
+initAccordion();
+initTabNav();
+initModal();
