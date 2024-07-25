7472c946f9a0976df75d28def1286b11091c976d
Geane Ramos <geaneramos1995@gmail.com>
2024-07-25 07:04:24 -0300

    feat: modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,22 @@
 export default function initModal() {
-    console.log('modal');
+  const modalContainer = document.querySelector('[data-modal="container"]');
+  const openModal = document.querySelector('[data-modal="abrir"]');
+  const closeModal = document.querySelector('[data-modal="fechar"]');
+
+  if (modalContainer && openModal && closeModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      modalContainer.classList.toggle("ativo");
+    }
+
+    function clickOutsideModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+
+    openModal.addEventListener("click", toggleModal);
+    closeModal.addEventListener("click", toggleModal);
+    modalContainer.addEventListener("click", clickOutsideModal);
+  }
 }
