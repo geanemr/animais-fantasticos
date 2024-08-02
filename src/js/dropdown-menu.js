f4681aa7f230fd333fe58af9e1986fbaf46ce5cb
Geane Ramos <geaneramos1995@gmail.com>
2024-08-02 07:51:45 -0300

    feat: handle outside clicks

--- src/js/dropdown-menu.js
+++ src/js/dropdown-menu.js
@@ -1,16 +1,19 @@
-export default function initDropdownMenu() {
+import outsideClick from "./outside-click.js";
 
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown]");
 
   dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
+    ["touchstart", "click"].forEach((userEvent) => {
       menu.addEventListener(userEvent, handleClick);
-    })
-  })
+    });
+  });
 
   function handleClick(event) {
     event.preventDefault();
-    this.classList.toggle('active');
+    this.classList.add("active");
+    outsideClick(this, ["touchstart", "click"], () => {
+      this.classList.remove("active");
+    });
   }
-  }
-  
+}
